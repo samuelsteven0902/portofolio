1d61f02cf7b6f532b38bc12e1df7eae1d603a5b1
samuelsteven0902 <samuelsteven@student.uns.ac.id>
2022-07-08 22:00:05 +0700

    menambah CV dan responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,20 +6,20 @@ import { Link } from 'react-scroll';
 const Home = () => {
   
   return (
-    <div name='home' className='w-full h-screen bg-[#041C32] flex justify-center'>
+    <div name='home' className='w-full h-screen bg-[#041C32] justify-center flex flex-wrap-reverse '>
       {/* Container */}
-      <div className='w-full mx-auto md:pl-32 flex flex-col justify-center h-full md:w-2/3'>
+      <div className='w-full mx-auto xl:pl-52  flex flex-col justify-center text-center lg:text-left h-1/2 lg:h-full lg:w-2/3 2xl:w-1/2 px-5 lg:pl-32'>
         <p className='text-[#ECB365]'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-[#ccd6f6]'>
           Samuel Steven
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Front-end Developer.
+        <h2 className='text-3xl sm:text-5xl md:text-5xl lg:text-5xl xxl:text-7xl font-bold text-[#8892b0]'>
+          I'm a <span> Front-end Developer.</span>
         </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        <p className='text-[#8892b0] py-4 max-w-[700px] mx-auto lg:mx-0'>
         Hello! I'm Samuel and currently I'm a student at Sebelas Maret University Associate Degree in software engineer who is interested in web development.
         </p>
-        <div>
+        <div className='flex justify-center lg:justify-start'>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
           <Link to='work' smooth={true} duration={500}>
             View Work
@@ -30,8 +30,8 @@ const Home = () => {
           </button>
         </div>
       </div>
-      <div className='w-full mx-auto px-8 md:pr-24 flex flex-col justify-center h-full md:w-1/3'>
-          <img className='w-[500px] mx-auto ' src={profile} alt="HTML icon" />         
+      <div className='w-full mx-auto px-8 flex flex-col justify-center h-1/2 lg:h-full 2xl:w-1/2 lg:w-1/3 pt-32 xl:pt-0 items-center'>
+          <img className='lg:w-[500px] mx-auto w-96' src={profile} alt="HTML icon" />         
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,7 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
+import resume from '../assets/CV_Samuel Steven Prisma Hasiyono.pdf'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -133,7 +134,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/' target='_blank'
+              href={resume}
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
